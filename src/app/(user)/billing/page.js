6cd21bc6f16d7bfb4da91b3eced55afd64bad32a
Talha Mughal <talhamughal52352@gmail.com>
2024-05-19 00:43:02 +0500

    Update page.js
    Add the FAQs Section in the billing Page

--- src/app/(user)/billing/page.js
+++ src/app/(user)/billing/page.js
@@ -8,8 +8,8 @@ const page = () => {
           Designed for business teams like yours
         </h2>
         <p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
-          Here at Jasper SEO we focus on markets where technology, innovation, and
-          capital can unlock long-term value and drive economic growth.
+          Here at Jasper SEO we focus on markets where technology, innovation,
+          and capital can unlock long-term value and drive economic growth.
         </p>
       </div>
       <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
@@ -341,6 +341,124 @@ const page = () => {
           >
             Activate
           </a>
+        </div>
+      </div>
+      <div class="relative w-full px-6 pt-10 pb-8 mt-8 shadow-xl sm:mx-auto sm:max-w-2xl sm:rounded-lg sm:px-10">
+        <div class="mx-auto px-5">
+          <div class="flex flex-col items-center">
+            <h2 class="mt-5 text-center text-3xl font-bold tracking-tight md:text-5xl">
+              FAQ
+            </h2>
+            <p class="mt-3 text-lg text-neutral-500 md:text-xl">
+              Frequenty Asked Questions
+            </p>
+          </div>
+          <div class="mx-auto mt-8 grid max-w-xl divide-y divide-neutral-200">
+            <div class="py-5">
+              <details class="group">
+                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                  <span>How do I purchase a package ?</span>
+                  <span class="transition group-open:rotate-180">
+                    <svg
+                      fill="none"
+                      height="24"
+                      shape-rendering="geometricPrecision"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="1.5"
+                      viewBox="0 0 24 24"
+                      width="24"
+                    >
+                      <path d="M6 9l6 6 6-6"></path>
+                    </svg>
+                  </span>
+                </summary>
+                <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+                  To purchase a package, Click on any package and enter your
+                  payment details. That's it.
+                </p>
+              </details>
+            </div>
+            <div class="py-5">
+              <details class="group">
+                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                  <span>Can I upgrade my package later ?</span>
+                  <span class="transition group-open:rotate-180">
+                    <svg
+                      fill="none"
+                      height="24"
+                      shape-rendering="geometricPrecision"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="1.5"
+                      viewBox="0 0 24 24"
+                      width="24"
+                    >
+                      <path d="M6 9l6 6 6-6"></path>
+                    </svg>
+                  </span>
+                </summary>
+                <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+                  Yes, you can upgrade your package at any time. Contact us in
+                  chat for upgrading it
+                </p>
+              </details>
+            </div>
+            <div class="py-5">
+              <details class="group">
+                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                  <span>What payment methods do you accept ?</span>
+                  <span class="transition group-open:rotate-180">
+                    <svg
+                      fill="none"
+                      height="24"
+                      shape-rendering="geometricPrecision"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="1.5"
+                      viewBox="0 0 24 24"
+                      width="24"
+                    >
+                      <path d="M6 9l6 6 6-6"></path>
+                    </svg>
+                  </span>
+                </summary>
+                <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+                  We accept various payment methods, including credit cards, and
+                  bank transfers. Contact us if you have any other method.
+                </p>
+              </details>
+            </div>
+            <div class="py-5">
+              <details class="group">
+                <summary class="flex cursor-pointer list-none items-center justify-between font-medium">
+                  <span>Can we refund if we don't like product ?</span>
+                  <span class="transition group-open:rotate-180">
+                    <svg
+                      fill="none"
+                      height="24"
+                      shape-rendering="geometricPrecision"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="1.5"
+                      viewBox="0 0 24 24"
+                      width="24"
+                    >
+                      <path d="M6 9l6 6 6-6"></path>
+                    </svg>
+                  </span>
+                </summary>
+                <p class="group-open:animate-fadeIn mt-3 text-neutral-600">
+                  Yes, You can refund within 3 days of purchase. Contact us in
+                  live support for refunding.
+                </p>
+              </details>
+            </div>
+          </div>
         </div>
       </div>
     </>
