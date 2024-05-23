18362fdecb9e39f3121b8373dd2dadc4fbc7a241
Talha Mughal <talhamughal52352@gmail.com>
2024-05-23 23:07:01 +0500

    Update UsersDataTable.js
    Add the Search Functionality in the data table

--- src/components/UsersDataTable.js
+++ src/components/UsersDataTable.js
@@ -1,6 +1,9 @@
 "use client";
+import { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 const UsersDataTable = ({ users }) => {
+  const [filteredUsers, setFilteredUsers] = useState(users);
+  const [search, setSearch] = useState("");
   const columns = [
     {
       name: "Name",
@@ -16,14 +19,33 @@ const UsersDataTable = ({ users }) => {
     },
   ];
 
+  useEffect(() => {
+    const result = users.filter((user) => {
+      return user.username.toLowerCase().includes(search.toLowerCase());
+    });
+
+    setFilteredUsers(result);
+  }, [search]);
+
   return (
     <DataTable
+      title="Users"
       columns={columns}
-      data={users}
+      data={filteredUsers}
       pagination
       highlightOnHover
       fixedHeader
       fixedHeaderScrollHeight="350px"
+      subHeader
+      subHeaderComponent={
+        <input
+          type="text"
+          placeholder="Search here"
+          value={search}
+          className="rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      }
     />
   );
 };
