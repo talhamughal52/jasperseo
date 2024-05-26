510ff7107401f66ae93fd34db7f42c7b95f2afac
Talha Mughal <talhamughal52352@gmail.com>
2024-05-26 22:05:45 +0500

    Update user.model.js

--- src/lib/database/models/user.model.js
+++ src/lib/database/models/user.model.js
@@ -38,6 +38,6 @@ const UserSchema = new Schema(
   }
 );
 
-const User = models?.customer || model("Customer", UserSchema);
+const User = models?.User || model("User", UserSchema);
 
 export default User;
