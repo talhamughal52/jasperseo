5e138ef348ab91e104c0012b526fdcb7124ca218
Talha Mughal <talhamughal52352@gmail.com>
2024-06-08 17:41:31 +0500

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
-  publicRoutes: ["/", "/about", "/pricing", "/contact", "/api/webhooks/clerk"],
+  publicRoutes: [
+    "/",
+    "/about",
+    "/pricing",
+    "/contact",
+    "/api/webhooks/clerk, '/api/webhooks/stripe'",
+  ],
 });
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
