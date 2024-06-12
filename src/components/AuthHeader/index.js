14bd673d94b9018d0d56bf6e9fed10d54d834ae6
Talha Mughal <talhamughal52352@gmail.com>
2024-06-13 01:31:31 +0500

    Update index.js

--- src/components/AuthHeader/index.js
+++ src/components/AuthHeader/index.js
@@ -1,7 +1,5 @@
 import Link from "next/link";
 import DarkModeSwitcher from "./DarkModeSwitcher";
-import DropdownMessage from "./DropdownMessage";
-import DropdownNotification from "./DropdownNotification";
 import DropdownUser from "./DropdownUser";
 import Image from "next/image";
 
@@ -66,8 +64,6 @@ const Header = (props) => {
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             <DarkModeSwitcher />
-            <DropdownNotification />
-            <DropdownMessage />
           </ul>
           <DropdownUser />
         </div>
